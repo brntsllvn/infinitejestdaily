658cbe0be54a7708397ee3fd5ae7bb4ecb498782
Brent J Sullivan <brntsllvn@gmail.com>
2015-10-10 19:22:21 +0000

    add Bootstrap

--- Gemfile
+++ Gemfile
@@ -8,10 +8,9 @@ gem 'rails', '4.2.0'
 gem 'pg'
 gem 'figaro' # secure env variables
 
+gem 'bootstrap-sass', '~> 3.3.5'
+gem 'sass-rails', '>= 3.2'
 
-
-# Use SCSS for stylesheets
-gem 'sass-rails', '~> 5.0'
 # Use Uglifier as compressor for JavaScript assets
 gem 'uglifier', '>= 1.3.0'
 # Use CoffeeScript for .coffee assets and views
--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,6 +1,6 @@
 class QuotesController < ApplicationController  
   http_basic_authenticate_with name: Figaro.env.un, password: Figaro.env.pw, except: [:index, :vote]
-  #   http_basic_authenticate_with name: ENV['un'], password: ENV['pw'], except: :index
+  # http_basic_authenticate_with name: ENV['un'], password: ENV['pw'], except: :index
 
   before_action :set_quote, only: [:vote, :edit, :update, :destroy]
 
